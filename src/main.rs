48f6e3f86a381b7e15eb5855865a863f8f676fa0
vrgio <71187316+vrgio@users.noreply.github.com>
2026-03-27 18:36:41 +0200

    setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init {
+        #[arg(short, long)]
+        instance: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Init { instance }) => {
+            println!("{}",&instance);
+        }
+        None => {
+            println!("nothing");
+        }
+    }
 }
